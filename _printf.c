c7394abdb2b745f140a0c83b8b41780ffb416e56
fayez bast <fayezbast15@gmai.com>
2025-01-21 19:03:08 +0000

    print f func

--- _printf.c
+++ _printf.c
@@ -1,110 +1,96 @@
-#include <stdarg.h>
-#include <unistd.h>
+/*
+ * File: _printf.c
+ * Auth: Brennan D Baraban
+ *       Michael Klein
+ */
+
+#include "holberton.h"
+
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
 
 /**
- * print_char - Prints a single character.
- * @args: The argument list.
- *
- * Return: The number of characters printed.
+ * cleanup - Peforms cleanup operations for _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
  */
-int print_char(va_list args)
+void cleanup(va_list args, buffer_t *output)
 {
-	char c = va_arg(args, int);
-
-	write(1, &c, 1);
-	return (1);
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
 }
 
 /**
- * print_string - Prints a string.
- * @args: The argument list.
+ * run_printf - Reads through the format string for _printf.
+ * @format: Character string to print - may contain directives.
+ * @output: A buffer_t struct containing a buffer.
+ * @args: A va_list of arguments.
  *
- * Return: The number of characters printed.
+ * Return: The number of characters stored to output.
  */
-int print_string(va_list args)
+int run_printf(const char *format, va_list args, buffer_t *output)
 {
-	char *str = va_arg(args, char *);
-	int i;
+	int i, wid, prec, ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
 
-	if (!str)
-		str = "(null)";
+	for (i = 0; *(format + i); i++)
+	{
+		len = 0;
+		if (*(format + i) == '%')
+		{
+			tmp = 0;
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+					&tmp);
+			len = handle_length(format + i + tmp + 1, &tmp);
 
-	for (i = 0; str[i] != '\0'; i++)
-		write(1, &str[i], 1);
-
-	return (i);
+			f = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flags, wid, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
+		}
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+	cleanup(args, output);
+	return (ret);
 }
 
 /**
- * print_percent - Prints a percent sign.
+ * _printf - Outputs a formatted string.
+ * @format: Character string to print - may contain directives.
  *
  * Return: The number of characters printed.
- */
-int print_percent(void)
-{
-	write(1, "%", 1);
-	return (1);
-}
-
-/**
- * handle_specifier - Handles format specifiers.
- * @specifier: The format specifier character.
- * @args: The argument list.
- *
- * Return: The number of characters printed.
- */
-int handle_specifier(char specifier, va_list args)
-{
-	if (specifier == 'c')
-		return (print_char(args));
-	if (specifier == 's')
-		return (print_string(args));
-	if (specifier == '%')
-		return (print_percent());
-
-	write(1, "%", 1);
-	write(1, &specifier, 1);
-	return (2);
-}
-
-/**
- * _printf - Produces output according to a format.
- * @format: A character string containing directives.
- *
- * Return: The number of characters printed (excluding null byte),
- *         or -1 for invalid input.
  */
 int _printf(const char *format, ...)
 {
-	int count = 0;
+	buffer_t *output;
 	va_list args;
-	const char *ptr;
+	int ret;
 
-	if (!format)
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
 		return (-1);
 
 	va_start(args, format);
 
-	for (ptr = format; *ptr != '\0'; ptr++)
-	{
-		if (*ptr == '%')
-		{
-			ptr++;
-			if (*ptr == '\0')
-			{
-				va_end(args);
-				return (-1); /* Invalid: single % at end */
-			}
-			count += handle_specifier(*ptr, args);
-		}
-		else
-		{
-			write(1, ptr, 1);
-			count++;
-		}
-	}
+	ret = run_printf(format, args, output);
 
-	va_end(args);
-
-	return (count);
+	return (ret);
 }
