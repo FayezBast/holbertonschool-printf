0a84ded3d2277988b665f3512d7b825a485df748
fayez bast <fayezbast15@gmai.com>
2025-01-21 17:56:23 +0000

    print f func

--- _printf.c
+++ _printf.c
@@ -10,66 +10,65 @@
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
-    const char *ptr;
+	int count = 0;
+	va_list args;
+	const char *ptr;
 
-    if (!format)
-        return (-1);
+	if (!format)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (ptr = format; *ptr != '\0'; ptr++)
-    {
-        if (*ptr == '%')
-        {
-            ptr++;
-            if (*ptr == '\0')
-            {
-                va_end(args);
-                return (-1); /* Invalid: single % at end */
-            }
-            if (*ptr == 'c')
-            {
-                char c = va_arg(args, int);
+	for (ptr = format; *ptr != '\0'; ptr++)
+	{
+		if (*ptr == '%')
+		{
+			ptr++;
+			if (*ptr == '\0')
+			{
+				va_end(args);
+				return (-1); /* Invalid: single % at end */
+			}
+			if (*ptr == 'c')
+			{
+				char c = va_arg(args, int);
 
-                write(1, &c, 1);
-                count++;
-            }
-            else if (*ptr == 's')
-            {
-                char *str = va_arg(args, char *);
-                int i;
+				write(1, &c, 1);
+				count++;
+			}
+			else if (*ptr == 's')
+			{
+				char *str = va_arg(args, char *);
+				int i;
 
-                if (!str)
-                    str = "(null)";
+				if (!str)
+					str = "(null)";
 
-                for (i = 0; str[i] != '\0'; i++)
-                    write(1, &str[i], 1);
+				for (i = 0; str[i] != '\0'; i++)
+					write(1, &str[i], 1);
 
-                count += i;
-            }
-            else if (*ptr == '%')
-            {
-                write(1, "%", 1);
-                count++;
-            }
-            else
-            {
-                write(1, "%", 1);
-                write(1, ptr, 1);
-                count += 2;
-            }
-        }
-        else
-        {
-            write(1, ptr, 1);
-            count++;
-        }
-    }
+				count += i;
+			}
+			else if (*ptr == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+			else
+			{
+				write(1, "%", 1);
+				write(1, ptr, 1);
+				count += 2;
+			}
+		}
+		else
+		{
+			write(1, ptr, 1);
+			count++;
+		}
+	}
 
-    va_end(args);
+	va_end(args);
 
-    return (count);
+	return (count);
 }
-
